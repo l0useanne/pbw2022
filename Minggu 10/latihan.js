d230dbb771085b47deeebd0d5dd32a0aa1dc1c40
l0useanne <111202214372@mhs.dinus.ac.id>
2022-11-28 19:16:08 +0700

    w
    g

---
+++ Minggu 10/latihan.js
@@ -0,0 +1,20 @@
+function MauGak(){
+    var nama = prompt("siapa nama kamu?", "");
+    alert("Ohh nama kamu " + nama);
+    alert("Aku mau ngomong sesuatu sama kamu " + nama);
+    alert("Sebenrnya udah lama aku mau ngmong ini ke kamu, tapi aku gaberani ngmong langsung sama kamu, jadi aku pake ini aja gapapa kan " + nama + (" ?"));
+    alert("Oke jadi gini....");
+    
+    if(confirm("Biarlah yang lalu menjadi masa laluku, namun untuk masa kini maukah kamu menjadi masa depanku " + nama + (" ?")) ==true){
+        alert("Makasih yaaa <33");
+        alert("besok kita date yaaaaa " + nama + (" !!"));
+        alert("sampai jumpa besokkkkk babaii <333");
+    } 
+    else{
+        for(i=0;i<9999;i++){
+            alert("yah sayang banget");
+            alert("Maaf kamu kena loop :p");
+            
+        } 
+    }
+}
